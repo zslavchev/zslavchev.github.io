db6d6b1d42d5bf91251696c58b5139a099b20e40
Zhoro Slavchev <zgslavchev@gmail.com>
2021-09-29 16:29:52 +0300

    fix link on some project, fix scroll reveal, include scrollreveal js into head for better performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,23 +36,23 @@ const sr = ScrollReveal({
 /* Scroll Home */
 sr.reveal('.home_title', {});
 sr.reveal('.button', { delay: 200 });
-sr.reveal('.home_img', { delay: 400 });
+sr.reveal('.home_img', { delay: 200 });
 sr.reveal('.home_social-icon', { interval: 200 });
 
 /* Scroll About */
 sr.reveal('.about_img', {});
-sr.reveal('.about_subtitle', { delay: 400 });
-sr.reveal('.about_text', { delay: 400 });
+sr.reveal('.about_subtitle', { delay: 200 });
+sr.reveal('.about_text', { delay: 200 });
 
 /* Scroll Skills */
 sr.reveal('.skills_subtitle', {});
 sr.reveal('.skills_text', {});
 sr.reveal('.skills_data', { interval: 200 });
-sr.reveal('.skills_img', { delay: 600 });
+sr.reveal('.skills_img', { delay: 200 });
 
 /* Scroll Work */
-sr.reveal('.work_img', { interval: 220 });
-sr.reveal('.work-description', { interval: 220 });
+sr.reveal('.work_img', { interval: 200 });
+sr.reveal('.work-description', { interval: 200 });
 
 /* Scroll Contact */
 sr.reveal('.contact_input', { interval: 200 });
